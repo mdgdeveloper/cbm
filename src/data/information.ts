49a5b402394e97d84ffff0556688fe377d6e52b3
Sergio Madrigal <smadrigal@unicef.org>
2024-01-21 01:41:42 -0500

    Header update

---
+++ src/data/information.ts
@@ -0,0 +1,14 @@
+interface InfoMSG {
+    title: string;
+    content: string;
+}
+
+export const infoMSG: InfoMSG = {
+    title: "Main",
+    content: "Descuentos en los productos de cuidado intensivo de la piel. Hasta 20%."
+}
+
+export const infoLocation: InfoMSG = {
+    title: "Main",
+    content: "Calle 7 # 6- 25. Buenavista del Norte. Tenerife."
+}
