0de4bbc56a3d04a073a18bf413b021abca76693d
sergiomadrigal <sergiomadrigal@gmail.com>
2024-02-07 20:53:46 +0100

    Hero v1 completed

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,13 @@ export default {
 		extend: {
 			fontFamily: {
 				birthstone: ['Birthstone'],
-			  },
+				urbanist: ['Urbanist Variable'],
+			},
+			fontSize: {
+				'10xl': '10rem',
+				'11xl': '12rem',
+				'12xl': '14rem',
+			},
 		},
 	},
 	plugins: [],
