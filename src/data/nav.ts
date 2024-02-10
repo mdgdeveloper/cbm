2e45f13825f4ae588beb78358e69666a491399c6
sergiomadrigal <sergiomadrigal@gmail.com>
2024-02-10 11:48:37 +0100

    Nav menu completed

--- src/data/nav.ts
+++ src/data/nav.ts
@@ -1,39 +1,45 @@
 interface Nav {
-    id: number;
-    name: string;
-    icon: string;
-    link: string;
+  id: number;
+  name: string;
+  icon: string;
+  link: string;
 }
 
-export const navigation:Nav[] = [
-    {
-      "id": 1,
-      "name": "Inicio",
-      "icon": "fa-home",
-      "link": "/"
-    },
-    {
-      "id": 2,
-      "name": "Servicios",
-      "icon": "fa-scissors",
-      "link": "/servicios"
-    },
-    {
-      "id": 3,
-      "name": "Contacto",
-      "icon": "fa-envelope",
-      "link": "/contacto"
-    },
-    {
-      "id": 4,
-      "name": "Nosotros",
-      "icon": "fa-users",
-      "link": "/nosotros"
-    },
-    {
-      "id": 5,
-      "name": "Reservas",
-      "icon": "fa-calendar",
-      "link": "/reservas"
-    }
+export const navigation: Nav[] = [
+  {
+    "id": 1,
+    "name": "Nosotros",
+    "icon": "fa-home",
+    "link": "/"
+  },
+  {
+    "id": 2,
+    "name": "Servicios",
+    "icon": "fa-scissors",
+    "link": "/servicios"
+  },
+  {
+    "id": 3,
+    "name": "Ofertas",
+    "icon": "fa-envelope",
+    "link": "/contacto"
+  },
+  {
+    "id": 4,
+    "name": "Galería",
+    "icon": "fa-users",
+    "link": "/nosotros"
+  },
+  {
+    "id": 5,
+    "name": "Eventos",
+    "icon": "fa-calendar",
+    "link": "/reservas"
+  },
+  {
+    "id": 6,
+    "name": "Contacto",
+    "icon": "fa-calendar",
+    "link": "/contacto"
+  }
 ]
